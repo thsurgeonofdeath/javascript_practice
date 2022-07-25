a735cfed90a5a50ebf6c71a352778eaf63c7f66f
thsurgeonofdeath <salahek25@gmail.com>
2022-07-25 14:37:28 +0100

    exercice 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  const celc = Math.round(((far - 32)*(5/9))*10)/10;
+  return celc;
 };
 
-const ctof = function() {
 
+const ctof = function(cel) {
+  const far = Math.round(((cel * (9/5))+32)*10)/10;
+  return far;
 };
 
 // Do not edit below this line
