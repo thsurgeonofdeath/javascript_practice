fbd187e3fc09e691397c20ecf38d110bbc05dce7
thsurgeonofdeath <salahek25@gmail.com>
2022-07-25 14:23:19 +0100

    exercice 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(one,two) {
 
+    let count = 0;
+    let temp = 0;
+    if(one<0 || two<0){
+        return 'ERROR';
+    }
+    if(!Number.isInteger(one) || !Number.isInteger(two)){
+        return 'ERROR';
+    }
+
+    if(one > two){
+        temp = two;
+        two = one;
+        one = temp;
+    }
+
+    for(let i=one; i<=two; i++){
+        count += i;
+    }
+
+    return count;
 };
 
 // Do not edit below this line
