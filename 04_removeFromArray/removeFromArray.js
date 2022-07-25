88e95fae617c7d47a6a485f17be46eb923a7dee6
thsurgeonofdeath <salahek25@gmail.com>
2022-07-25 14:10:23 +0100

    exercice 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
+    const array = args[0];
+    const newArray = [];
+    array.forEach(element => {
+        if(!args.includes(element)){
+            newArray.push(element);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
