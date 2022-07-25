04c97ff784f620f912797ba9655c2a88efc4f43c
thsurgeonofdeath <salahek25@gmail.com>
2022-07-25 13:50:21 +0100

    exercice 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
+const reverseString = function(wordToResverse) {
 
+    reversedWord = wordToResverse.split('').reverse().join('');
+    return reversedWord;
 };
 
 // Do not edit below this line
