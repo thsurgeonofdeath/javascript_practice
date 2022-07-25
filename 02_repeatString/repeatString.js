dea42f0e472fde158ec2166d31e560e6af5b548a
thsurgeonofdeath <salahek25@gmail.com>
2022-07-25 13:40:31 +0100

    second exercice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word,number) {
 
+    if(number < 0) return "ERROR";
+    let repeat ="";
+    for (i=0; i<number; i++){
+        repeat = repeat.concat(word);
+    }
+
+    return repeat;
 };
 
 // Do not edit below this line
